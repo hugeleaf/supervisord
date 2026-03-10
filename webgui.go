e0a6894542a084398d2f206540705e7abd2a7325
hugeleaf <c00l@163.com>
2026-03-10 09:41:09 +0800

    embed web gui

--- webgui.go
+++ webgui.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"net/http"
-
+	"embed"
 	"github.com/gorilla/mux"
 )
+
+//go:embed webgui/*
+var WebFlies embed.FS
 
 // SupervisorWebgui the interface to show a WEBGUI to control the supervisor
 type SupervisorWebgui struct {
@@ -18,8 +21,14 @@ func NewSupervisorWebgui(supervisor *Supervisor) *SupervisorWebgui {
 	return &SupervisorWebgui{router: router, supervisor: supervisor}
 }
 
+func WebGuiHandler(w http.ResponseWriter, r *http.Request) {
+        http.Redirect(w, r, "/webgui", http.StatusMovedPermanently)
+}
+
 // CreateHandler create a http handler to process the request from WEBGUI
 func (sw *SupervisorWebgui) CreateHandler() http.Handler {
-	sw.router.PathPrefix("/").Handler(http.FileServer(HTTP))
+	// sw.router.PathPrefix("/").Handler(http.FileServer(HTTP))
+    sw.router.HandleFunc("/", WebGuiHandler)
+	sw.router.PathPrefix("/webgui").Handler(http.FileServer(http.FS(WebFlies)))
 	return sw.router
 }
